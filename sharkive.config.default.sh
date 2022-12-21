cce11947003b3ef7823d9a8bcc8cda52883e57e3
gabl <99302669+gabldotink@users.noreply.github.com>
2022-12-20 17:05:56 -0700

    I did some stuff

---
+++ sharkive.config.default.sh
@@ -0,0 +1,2 @@
+# sharkive configuration
+#
--- sharkive.sh
+++ sharkive.sh
@@ -2,6 +2,22 @@
 # sharkive [https://github.com/gabldotink/sharkive/]
 # CC0 Public Domain [https://creativecommons.org/publicdomain/zero/1.0/]
 #
-# sharkive is a bash script to archive online content,
+# sharkive is a bash script to download online content,
 # and optionally upload it to the Internet Archive [https://archive.org].
 
+# import config file
+source ./sharkive.config.sh
+
+# text for -h flag
+sharkive_help () {
+	printf "You're using sharkive; a bash script to download online content,\n"
+	printf "and optionally upload it to the Internet Archive.\n"
+	printf "\n"
+}
+
+# get flags
+while getopts 'h' flag; do
+	case "${flag}" in
+		h) sharkive_help
+	esac
+done
