8f116b0966ee0c0194effc5045ffa7e975004069
gabldotink <gabl@gabl.ink>
2023-03-12 20:03:43 -0600

    changes some printf double-quoted strings to single quotes fixes some other stuff sharkive still is not capable of ordering hamburgers from wendy's. please be patient while I work on it

--- sharkive.sh
+++ sharkive.sh
@@ -28,43 +28,30 @@ source './sharkive.config.default.sh'
 
 # invalid flags message
 flags_invalid() {
-	printf "invalid flags given\n"
+	printf 'invalid flags given\n'
 }
 
 # short usage text
 usage_short() {
-	printf "one of the flags [-hm] must be used\n"
-	printf "use 'sharkive -h' for detailed help\n"
+	printf 'one of the flags [-hm] must be used\n'
+	printf 'use ‘sharkive -h’ for detailed help\n'
 }
 
 # usage text
 usage() {
-	printf "you're using sharkive; a bash script to download online content,\n"
-	printf "and optionally upload it to the Internet Archive.\n"
-	printf "\n"
-	printf "flags:\n"
-	printf "  -h  print this help message\n"
-	printf "  -m  define “method” for downloading\n"
-	printf "  -s  define download source (URI)\n"
-	printf "      (use multiple times for multiple sources)\n"
-	printf "  -u  upload to the Internet Archive once download is finished\n"
-}
+	cat << 'end'
+usage: sharkive [-h] [-m method -s source] [-u]
 
-# check if programs are installed
-check_commands() {
-	if command -v ia &>/dev/null; then
-	declare -l ia_exists='yep'; fi
-	#if command -v ytarchive &>/dev/null; then
-	#declare -l ytarchive_exists='yep'; fi
-	#if command -v youtube-dl &>/dev/null; then
-	#declare -l ytdl_exists='yep'; fi
-	if command -v yt-dlp &>/dev/null; then
-	declare -l ytdlp_exists='yep'; fi
-	#if command -v you-get &>/dev/null; then
-	#declare -l youget_exists='yep'; fi
-	if command -v ffmpeg &>/dev/null \
-	&& command -v ffprobe &>/dev/null; then
-	declare -l ffmpeg_exists='yep'; fi
+you’re using sharkive; a bash script to download online content,
+and optionally upload it to the Internet Archive.
+
+flags:
+  -h  print this help message
+  -m  define “method” for downloading
+  -s  define download source (URI)
+      (use multiple times for multiple sources)
+  -u  upload to the Internet Archive once download is finished
+end
 }
 
 # get flags
@@ -72,19 +59,19 @@ while getopts ':hm:s:u' flag; do
 	case "${flag}" in
 	h) # help
 		usage >&2
-		exit 0
+		exit 2
 		;;
 	m) # download method
 		declare -l method="${OPTARG}" # store value as lowercase
-		printf "using method %s\n" "${method}"
+		printf 'using method %s\n' "${method}"
 		;;
 	s) # download source
-		dl_source+=("${OPTARG}")
-		printf "using source %s\n" "${dl_source[*]}"
+		declare dl_source+=("${OPTARG}")
+		printf 'using source %s\n' "${dl_source[*]}"
 		;;
 	u) # upload
-		printf "uploading to the Internet Archive when finished\n"
-		should_upload='yep'
+		printf 'uploading to the Internet Archive when finished\n'
+		declare -l should_upload='yep'
 		;;
 	*) # invalid flags
 		flags_invalid >&2
@@ -101,22 +88,39 @@ if [ "${flags_present}" != 'yep' ]; then
 	exit 1
 fi
 
+# check if programs are installed
+check_commands() {
+	if command -v ia &> /dev/null; then
+	declare -l ia_exists='yep'; fi
+	#if command -v ytarchive &> /dev/null; then
+	#declare -l ytarchive_exists='yep'; fi
+	#if command -v youtube-dl &> /dev/null; then
+	#declare -l ytdl_exists='yep'; fi
+	if command -v yt-dlp &> /dev/null; then
+	declare -l ytdlp_exists='yep'; fi
+	#if command -v you-get &> /dev/null; then
+	#declare -l youget_exists='yep'; fi
+	if command -v ffmpeg &> /dev/null \
+	&& command -v ffprobe &> /dev/null; then
+	declare -l ffmpeg_exists='yep'; fi
+}
+
 # archive from youtube
 if [ "${method}" == 'youtube' ]; then
 	check_commands
 	if [ "${ytdlp_exists}" != 'yep' ]; then
-		printf "error: yt-dlp is not installed\n" >&2
+		printf 'error: yt-dlp is not installed\n' >&2
 		to_exit='yep'
 	fi
 	if [ "${ffmpeg_exists}" != 'yep' ]; then
-		printf "error: ffmpeg is not installed\n" >&2
+		printf 'error: ffmpeg is not installed\n' >&2
 		to_exit='yep'
 	fi
 	if [ "${to_exit}" == 'yep' ]; then
-		printf "error: required applications are not installed. exiting\n" >&2
+		printf 'error: required applications are not installed. exiting\n' >&2
 		exit 1
 	fi
-	printf "required applications are installed\n"
+	printf 'required applications are installed\n'
 
 	if [ -n "${dl_source[0]}" ]; then
 		declare -i failure_retries_two="${failure_retries}"
@@ -133,15 +137,15 @@ if [ "${method}" == 'youtube' ]; then
 		--sleep-subtitles 0 --write-description \
 		--write-thumbnail --write-all-thumbnails --no-overwrites \
 		--ignore-no-formats-error --no-windows-filenames \
-		--extractor-args "youtube:player_client=all;include_incomplete_formats" \
+		--extractor-args 'youtube:player_client=all;include_incomplete_formats' \
 		--output "${dl_location}/youtube/%(id)s/%(id)s.%(format_id)s.%(ext)s"
 		do if [ "${failure_retries}" -gt 0 ]; then
 				# these go to stdout because it makes more sense when redirecting
-				printf "unsuccessful download.\n"
-				printf "retrying %s more times\n" "$failure_retries"
+				printf 'unsuccessful download.\n'
+				printf 'retrying %s more times\n' "$failure_retries"
 				failure_retries="$((failure_retries-1))"
 			else
-				printf "no more retries. exiting\n"
+				printf 'no more retries. exiting\n'
 				exit 1
 			fi
 		done
@@ -158,18 +162,18 @@ if [ "${method}" == 'youtube' ]; then
 			--sleep-subtitles 0 \
 			--embed-thumbnail --get-comments \
 			--embed-metadata --embed-chapters --embed-info-json \
-			--extractor-args "youtube:player_client=all" --no-windows-filenames \
+			--extractor-args 'youtube:player_client=all' --no-windows-filenames \
 			--output "${dl_location}/youtube/%(id)s/full/%(title)s.%(id)s.%(ext)s"
 			do if [ "${failure_retries_two}" -gt 0 ]; then
-					printf "unsuccessful download.\n"
-					printf "retrying %s more times\n" "$failure_retries_two"
+					printf 'unsuccessful download.\n'
+					printf 'retrying %s more times\n' "$failure_retries_two"
 					failure_retries_two="$((failure_retries_two-1))"
 				else
-					printf "no more retries. exiting\n"
+					printf 'no more retries. exiting\n'
 					exit 1
 				fi
 			done
 		fi
 	fi
-	printf "\n\ndownload successful\n"
+	printf '\n\ndownload successful\n'
 fi
