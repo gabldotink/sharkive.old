e7c180b34b92c9a708b9363b80f2ff2e7970e11a
gabldotink <gabl@gabl.ink>
2023-03-22 18:05:01 -0600

    last line is actually broken but I'm committing anyway

--- sharkive.sh
+++ sharkive.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-flags_invalid() {
+flags_invalid(){
 printf 'invalid flags given\n'
-};usage_short() {
+};usage_short(){
 printf 'one of the flags [-hm] must be used\n';printf 'use ‘sharkive -h’ for detailed help\n'
-};usage() {
+};usage(){
 cat << 'eof'
 usage: sharkive [-h] [-m method -s source] [-u]
 
@@ -33,45 +33,4 @@ eof
 done
 if [[ "${flags_present}" != 'yep' ]];then usage_short>&2;exit 1;fi;check_commands() {
 if command -v ia&>/dev/null;then declare -g ia_exists='yep';fi;if command -v yt-dlp&>/dev/null;then declare -g ytdlp_exists='yep';fi;if command -v ffmpeg&>/dev/null && command -v ffprobe&>/dev/null;then declare -g ffmpeg_exists='yep';fi
-};if [[ "${method}" == 'youtube' ]];then check_commands;if [[ "${ytdlp_exists}" != 'yep' ]]; then printf '[error] yt-dlp is not installed\n'>&2;declare to_exit='yep';fi;if [[ "${ffmpeg_exists}" != 'yep' ]];then printf '[error] ffmpeg is not installed\n'>&2;declare to_exit='yep';fi
-	if [[ "${to_exit}" == 'yep' ]]; then
-		printf '[error] required applications are not installed. exiting\n' >&2
-		exit 1
-	fi
-	printf 'required applications are installed\n'
-
-	if [[ -n "${dl_source[0]}" ]]; then
-		# download raw data
-		until yt-dlp "${dl_source[@]}" \
-		--ignore-config --use-extractors youtube \
-		--all-formats --allow-unplayable-formats \
-		--concurrent-fragments 1 \
-		--keep-fragments --abort-on-unavailable-fragment \
-		--get-comments --verbose \
-		--write-info-json --clean-info-json --no-continue \
-		--write-subs --write-auto-subs --sub-langs all \
-		--sleep-subtitles 0 --write-description \
-		--write-thumbnail --write-all-thumbnails --no-overwrites \
-		--ignore-no-formats-error --no-windows-filenames --no-restrict-filenames \
-		--extractor-args 'youtube:player_client=all;include_incomplete_formats' \
-		--output "${HOME}/.sharkive/dl/youtube/%(id)s/data/youtube-%(id)s.%(format_id)s.%(ext)s"
-		do printf '[info] ran into an error, going again\n'; done
-	fi
-		# now make a bv+ba video with attachments
-		until yt-dlp "${dl_source[@]}" \
-		--format bv+ba
-		--ignore-config --use-extractors youtube \
-		--concurrent-fragments 1 \
-		--abort-on-unavailable-fragment \
-		--embed-info-json --clean-info-json --no-continue \
-		--embed-subs --sub-langs all --verbose \
-		--sleep-subtitles 0 \
-		--embed-thumbnail --get-comments \
-		--embed-metadata --embed-chapters --embed-info-json \
-		--extractor-args 'youtube:player_client=all' --no-windows-filenames --no-restrict-filenames \
-		--output "${HOME}/.sharkive/dl/youtube/%(id)s/%(title)s.%(id)s.%(ext)s"
-			do printf '[info] ran into an error, going again\n'; done
-		fi
-	fi
-	printf '\n\n[info] download successful\n'
-fi
+};if [[ "${method}" == 'youtube' ]];then check_commands;if [[ "${ytdlp_exists}" != 'yep' ]]; then printf '[error] yt-dlp is not installed\n'>&2;declare to_exit='yep';fi;if [[ "${ffmpeg_exists}" != 'yep' ]];then printf '[error] ffmpeg is not installed\n'>&2;declare to_exit='yep';fi;if [[ "${to_exit}" == 'yep' ]];then;printf '[error] required applications are not installed. exiting\n'>&2;exit 1;fi;printf 'required applications are installed\n';if [[ -n "${dl_source[0]}" ]];then;until yt-dlp "${dl_source[@]}" --ignore-config --use-extractors youtube --all-formats --allow-unplayable-formats --concurrent-fragments 1 --keep-fragments --abort-on-unavailable-fragment --get-comments --verbose --write-info-json --clean-info-json --no-continue --write-subs --write-auto-subs --sub-langs all --sleep-subtitles 0 --write-description --write-thumbnail --write-all-thumbnails --no-overwrites --ignore-no-formats-error --no-windows-filenames --no-restrict-filenames --extractor-args 'youtube:player_client=all;include_incomplete_formats' --output "${HOME}/.sharkive/dl/youtube/%(id)s/data/youtube-%(id)s.%(format_id)s.%(ext)s";do printf '[info] ran into an error, going again\n';done;fi;until yt-dlp "${dl_source[@]}" --format bv+ba --ignore-config --use-extractors youtube --concurrent-fragments 1 --abort-on-unavailable-fragment --embed-info-json --clean-info-json --no-continue --embed-subs --sub-langs all --verbose --sleep-subtitles 0 --embed-thumbnail --get-comments --embed-metadata --embed-chapters --embed-info-json --extractor-args 'youtube:player_client=all' --no-windows-filenames --no-restrict-filenames --output "${HOME}/.sharkive/dl/youtube/%(id)s/%(title)s.%(id)s.%(ext)s";do printf '[info] ran into an error, going again\n';done;fi;fi;printf '\n\n[info] download successful\n';fi
